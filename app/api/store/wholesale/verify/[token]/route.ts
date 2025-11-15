de76c43d2ec0275bb9eefeba887f9d1b3ba3302f
James BONUTTO <jamesbonutto@Jamess-MBP.home>
2025-11-14 18:41:29 -0800

    Fix Store field name: 'name' -> 'storeName'

--- app/api/store/wholesale/verify/[token]/route.ts
+++ app/api/store/wholesale/verify/[token]/route.ts
@@ -21,7 +21,7 @@ export async function GET(
           select: {
             id: true,
             storeId: true,
-            name: true,
+            storeName: true,
           },
         },
       },
